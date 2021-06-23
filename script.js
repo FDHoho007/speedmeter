4384b45e72881e9374ac2fcb113f6ff0d297b690
FDHoho007 <mail@fdhoho007.de>
2021-06-23 15:50:26 +0200

    Added Min/Max Table.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 graphql("https://speed-api.myfdweb.de", "{ results(limit: 168) { time ping download upload packetLoss } }", null, null, function (data) {
-    let labels = [], download = [], upload = [], ping = [];
-    for (let result of data.data.results) {
+    let labels = [], download = [], upload = [], ping = [], min = [-1, -1, -1], max = [-1, -1, -1];
+    function minmax(index, value) {
+		if(value < min[index] || min[index] === -1)
+			min[index] = value;
+		if(value > max[index] || max[index] === -1)
+			max[index] = value;
+	}
+	for (let result of data.data.results) {
         let d = new Date(result.time * 1000);
         labels.push(d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes());
-        download.push(Math.floor(result.download / 100000) / 10);
-        upload.push(Math.floor(result.upload / 100000) / 10);
+        let dl = Math.floor(result.download / 100000) / 10;
+		download.push(dl);
+		minmax(0, dl);
+		let ul = Math.floor(result.upload / 100000) / 10;
+        upload.push(ul);
+		minmax(1, ul);
         ping.push(result.ping);
+		minmax(2, result.ping);
     }
     labels.reverse();
     download.reverse();
     upload.reverse();
     ping.reverse();
+	document.getElementById("download-min").innerText = min[0];
+	document.getElementById("download-max").innerText = max[0];
+	document.getElementById("upload-min").innerText = min[1];
+	document.getElementById("upload-max").innerText = max[1];
+	document.getElementById("ping-min").innerText = min[2];
+	document.getElementById("ping-max").innerText = max[2];
     new Chart(document.getElementById("chart").getContext("2d"), {
         type: "line",
         data: {
